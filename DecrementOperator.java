6d235981f4286fe06334b8f08a2bc7dcdd2e9bec
karunya006 <141421495+karunya006@users.noreply.github.com>
2023-08-07 21:25:02 +0530

    Add files via upload

---
+++ DecrementOperator.java
@@ -0,0 +1,16 @@
+package techzenure;
+
+public class DecrementOperator {
+
+	public static void main(String[] args) {
+		int k=20;
+		System.out.println(k--);
+		System.out.println(--k);
+		int n=60;
+		n=--n+n--;
+		System.out.println(n);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ welcome.java
@@ -0,0 +1,13 @@
+package techzenure;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("welcome to java traning");     
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
